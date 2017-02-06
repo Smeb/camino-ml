18cc4266066b94fd451ecee0ec42fd711386932c
Smeb <ben.ryves@red-badger.com>
2017-02-06 15:49:33 +0000

    Adds minutes for monday meeting

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ config_file_path = os.path.abspath(root_path + '/config.json')
 datasynth_path = os.path.abspath(root_path + '/camino/bin/datasynth')
 scheme_path = os.path.abspath(root_path + '/PGSE_90.scheme')
 data_path = os.path.abspath(root_path + '/data')
-dataset_size = 1
+dataset_size = 10

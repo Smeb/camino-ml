2d18f83dfc83e6bc2f5ae302d833c2396d6fdf83
Smeb <ben.ryves@red-badger.com>
2017-04-06 16:44:50 +0100

    Changes training to be simpler

--- src/fitting/extract_parameters.py
+++ src/fitting/extract_parameters.py
@@ -1,14 +1,14 @@
 from src.config import MODELS
-from src.datasets.dataset_factory import CAMINO_COMPARTMENTS, get_model_name, get_parameter_names
+from src.datasets.dataset_factory import CAMINO_COMPARTMENTS, get_model_name, get_param_names
 
 def extract_all():
-  pass
+    pass
 
 def extract_model(model):
-  model_name = get_model_name(model)
-  parameter_names = get_parameter_names()
-  with open(f_path, 'r') as open_file:
-    lines = open_file.readlines()
+    model_name = get_model_name(model)
+    parameter_names = get_param_names()
+    with open(f_path, 'r') as open_file:
+        lines = open_file.readlines()
 
 def extract_fit():
-  pass
+    pass
--- src/machine_learning/algorithms/random_forest.py
+++ src/machine_learning/algorithms/random_forest.py
@@ -12,10 +12,11 @@ def train_rf(dataset):
         "max_depth":[3, 5, None], # max depth of tree, None = deep as required
         "bootstrap": [True, False], # if bootstrap samples are used for trees
         "criterion": ["mse"], # mean square error, mean absolute error
-        "n_estimators": [5, 10, 20, 40, 80] # number of trees to train
+        "n_estimators": [40, 80] # number of trees to train
     }
 
-    rf_reg = RandomForestRegressor()
-    grid_search = GridSearchCV(rf_reg, param_grid, n_jobs=multiprocessing.cpu_count(), verbose=10)
-    grid_search.fit(dataset.train_x, dataset.train_y)
-    return grid_search
+    rf_reg = RandomForestRegressor(n_estimators=100, max_depth=20, bootstrap=True, criterion="mse")
+    rf_reg.fit(dataset.train_x, dataset.train_y)
+    # grid_search = GridSearchCV(rf_reg, param_grid, n_jobs=multiprocessing.cpu_count(), verbose=10)
+    # grid_search.fit(dataset.train_x, dataset.train_y)
+    return rf_reg
